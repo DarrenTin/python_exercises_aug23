75ec92e8ebca24dc7a02ec1c8de6db9be01af5c5
DarrenTin <shan.tin2003a15@gmail.com>
2023-08-25 11:57:42 +0800

    Little change to e01

--- exercise_01.py
+++ exercise_01.py
@@ -1,7 +1,18 @@
-def convert_to_bits(n):
-    # convert to binary
+def convert_to_bin(n):
     return bin(n)
 
-# Test your function
+
+def count_one_in_bits(n):
+    count = 0
+    count_index = 0
+    converted = str(convert_to_bin(n))
+    for i in converted:
+        if converted[count_index] == '1':
+            count += 1
+        count_index += 1
+    return count
+
+
 num = 9876
-print(convert_to_bits(num))
+print(f"Bin: {convert_to_bin(num)}")
+print(f'1 in {convert_to_bin(num)} = {count_one_in_bits(num)}')
